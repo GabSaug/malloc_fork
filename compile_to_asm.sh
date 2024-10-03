f6a4f47996dce9dbaa658ebdc71c92edb53e04b9
SAUGER Gabriel <gabriel.sauger@loria.fr>
2024-10-03 15:32:23 +0200

    Added option for gcc for preprocessed  sourcefiles

--- compile_to_asm.sh
+++ compile_to_asm.sh
@@ -1,5 +1,5 @@
 opt="$(echo $3 | sed -e "s/-O0/$(cat /etc/gcc.opt)/g") -Wno-error -finline-limit=2"
-if ! gcc -Wall -Wextra -pedantic -std=c99 -I../include -masm=intel -fPIC -fvisibility=hidden "$1" -S -o "$2" $opt; then
+if ! gcc -x cpp-output -Wall -Wextra -pedantic -std=c99 -I../include -masm=intel -fPIC -fvisibility=hidden "$1" -S -o "$2" $opt; then
 	echo "Error compiling to asm $opt - malloc"
 	exit 1
 fi
