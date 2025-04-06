356cd2f4d6812d070085a3e19d53a94108667ddf
SAUGER Gabriel <gabriel.sauger@loria.fr>
2025-04-06 21:13:40 +0200

    Add -fno-inline option to compile_to_asm.sh

--- compile_to_asm.sh
+++ compile_to_asm.sh
@@ -1,4 +1,4 @@
-opt="$(echo $3 | sed -e "s/-O0/$(cat /etc/gcc.opt)/g") -Wno-error -finline-limit=2"
+opt="$(echo $3 | sed -e "s/-O0/$(cat /etc/gcc.opt)/g")  -fno-inline -Wno-error -finline-limit=2"
 if ! gcc -x cpp-output -Wall -Wextra -pedantic -std=c99 -I../include -masm=intel -fPIC -fvisibility=hidden "$1" -S -o "$2" $opt; then
 	echo "Error compiling to asm $opt - malloc"
 	exit 1
